302df62d3d3c80f03e1c5254a1673348f320bc83
Talin Senner <senner@users.noreply.github.com>
2026-02-05 11:03:05 -0600

    Update __init__.py

--- whitelabel/__init__.py
+++ whitelabel/__init__.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 try:
 import frappe
 
-__version__ = '0.0.2'
+__version__ = '0.0.1'
 
 if frappe.conf and frappe.conf.get("app_logo_url"):
     __logo__ = frappe.conf.get("app_logo_url") or '/assets/whitelabel/images/whitelabel_logo.jpg'
