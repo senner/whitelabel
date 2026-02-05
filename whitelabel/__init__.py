7e0717f95c6467a11d3955edfa8fefdc99efb72e
Talin Senner <senner@users.noreply.github.com>
2026-02-05 11:02:38 -0600

    Update __init__.py

--- whitelabel/__init__.py
+++ whitelabel/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
+try:
 import frappe
 
 __version__ = '0.0.1'
@@ -8,3 +10,7 @@ if frappe.conf and frappe.conf.get("app_logo_url"):
     __logo__ = frappe.conf.get("app_logo_url") or '/assets/whitelabel/images/whitelabel_logo.jpg'
 else:
     __logo__ = '/assets/whitelabel/images/whitelabel_logo.jpg'
+
+except Exception:
+# During pip install, or if frappe is unavailable
+pass`
